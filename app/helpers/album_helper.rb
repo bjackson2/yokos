5365ba079c5a4f525aa53ba0b0778dd05642d2f9
Ben Jackson <bjackson2@gmail.com>
2023-10-03 13:11:02 -0400

    Album name display format change

--- app/helpers/album_helper.rb
+++ app/helpers/album_helper.rb
@@ -6,4 +6,10 @@ module AlbumHelper
 
     "#{album.artist.name} - #{album.title}"
   end
+
+  def album_search_display_name(album)
+    return '' unless album
+
+    "#{album.title} - #{album.artist.name}"
+  end
 end
